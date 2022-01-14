6275a128754e23519023b63bf05cf81a36eb2db1
Madara Uchiha <grezhesyak2016@mail.ru>
2022-01-14 23:44:06 +0300

    init

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class solution
+    {
+        public void Stack( string s)
+        {
+            int fullness = 0;
+            int lenght = s.Count();
+
+            Stack<char> stack = new Stack<char>();
+            for(int i = 0; i < lenght; i ++)
+            {
+                char firstbracket = s[i];
+                if(firstbracket == '(' || firstbracket =='['|| firstbracket == '{')
+                {
+                    stack.Push(firstbracket);
+                    fullness++;
+                }
+                else
+                {
+                    char endbracket;
+                    if (fullness > 0)
+                    {
+                        endbracket = stack.Peek();
+                        stack.Pop();
+                        fullness--;
+                    }
+                    else 
+                    { 
+                        Console.WriteLine("Последовательность не правильная"); 
+                        break;
+                    }
+                    if(endbracket == '(' && firstbracket == ')'|| endbracket == '{' && firstbracket == '}'|| endbracket == '[' && firstbracket == ']')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Последовательность не правильная");
+                        break;
+                    }
+                    
+                }
+            }
+            if(fullness == 0)
+            {
+                Console.WriteLine("Правильная последовательность");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность не правильная");
+            }
+
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sol = new solution();
+            Console.WriteLine("Напиши последовательность скобок");
+            string s = Console.ReadLine();
+            sol.Stack(s);
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class solution
+    {
+        public void Year(int year)
+        {
+            if(year % 4 == 0 ||year % 400 == 0 && year % 100 !=0 )
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Год невисокосный");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год для проверки");
+            int year;
+            year = Convert.ToInt32(Console.ReadLine());
+            var sol = new solution();
+            sol.Year(year);
+        }
+    }
+}
